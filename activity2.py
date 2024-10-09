acc4d6ca8a23777eb72be241ebc3d927f9d88529
mustafa276 <mmustafamajeed1@gmail.com>
2024-10-09 12:37:32 +0500

    country code

---
+++ activity2.py
@@ -0,0 +1,5 @@
+country_number = {"pakistan" : 92}
+print("country code for pakistan")
+print(country_number.get, "pakistan", "not found")
+print("country code for america")
+print(country_number.get, "america", "not found")
